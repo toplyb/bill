f2b96532ff569cca785659f58f8e9785aad38ee5
xiaosheng <18337079291@163.com>
2023-07-17 16:56:49 +0800

    fix: 处理图表页面金额错误问题

--- src/utils/chart.ts
+++ src/utils/chart.ts
@@ -1,5 +1,6 @@
 import type { ButtonType, IChartOption } from '@/types/chart'
 import { getAllBills } from '@/utils/bill'
+import { handleMoney } from '@/utils/money'
 
 const monthData: IChartOption = {
   categories: [],
@@ -28,7 +29,7 @@ const yearData: IChartOption = {
   ]
 }
 
-// 获取到所有的月份和年份
+// 获取到所有的月份和年份以及对应的数据
 const getAllMonthOrAllYear = (type: ButtonType) => {
   const billList = getAllBills()
   const tempData = {} as any
@@ -38,25 +39,27 @@ const getAllMonthOrAllYear = (type: ButtonType) => {
     if (type === 'year') {
       const year = dateString[0]
       if (tempData[year]) {
-        tempData[year][item.type] += Number(item.money)
+        tempData[year][item.type] += Number(item.money) * 100
       } else {
         tempData[year] = {
-          expense: item.type === 'expense' ? Number(item.money) : 0,
-          income: item.type === 'income' ? Number(item.money) : 0
+          expense: item.type === 'expense' ? Number(item.money) * 100 : 0,
+          income: item.type === 'income' ? Number(item.money) * 100 : 0
         }
       }
     } else if (type === 'month') {
       const month = dateString[1]
       if (tempData[month]) {
-        tempData[month][item.type] += Number(item.money)
+        tempData[month][item.type] += Number(item.money) * 100
       } else {
         tempData[month] = {
-          expense: item.type === 'expense' ? Number(item.money) : 0,
-          income: item.type === 'income' ? Number(item.money) : 0
+          expense: item.type === 'expense' ? Number(item.money) * 100 : 0,
+          income: item.type === 'income' ? Number(item.money) * 100 : 0
         }
       }
     }
   })
+
+  console.log(tempData, 'tempData==')
 
   // 月份或年份排序
   const categories = Object.keys(tempData).sort()
@@ -64,14 +67,14 @@ const getAllMonthOrAllYear = (type: ButtonType) => {
   for (let category of categories) {
     data[category] = tempData[category]
   }
-
+  console.log(data, 'data===')
   if (type === 'month') {
     monthData.categories = categories
     const allIncome: string[] = []
     const allExpense: string[] = []
     monthData.categories.forEach(month => {
-      allIncome.push(data[month]['income'])
-      allExpense.push(data[month]['expense'])
+      allIncome.push(handleMoney(data[month]['income']) + '')
+      allExpense.push(handleMoney(data[month]['expense']) + '')
     })
     monthData['series'][1]['data'] = allExpense
     monthData['series'][0]['data'] = allIncome
---
+++ src/utils/money.ts
@@ -0,0 +1,7 @@
+const handleMoney = (money: number) => {
+  return Math.round(money) / 100
+}
+
+export {
+  handleMoney
+}
